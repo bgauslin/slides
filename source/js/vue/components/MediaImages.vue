4adf9601ca2884ab1b5d98cc462b724e484d2b88
Ben Gauslin <ben@gauslin.com>
2018-08-18 05:46:35 -0400

    SingleImage component created

--- source/js/vue/components/MediaImages.vue
+++ source/js/vue/components/MediaImages.vue
@@ -1,81 +1,30 @@
 <template lang="pug">
   div.images
-    figure.image(
+    div.images__group(
       v-for="image in images",
     )
-      img(
-        :alt="alt(image)",
-        :src="src(image)",
-        :srcset="srcset(image)",
+      single-image(
+        :image="image",
       )
 </template>
 
 <script>
-import imagesLoaded from 'imagesloaded';
-import ImageFetcher from './ImageFetcher.vue';
+
+import SingleImage from './SingleImage.vue';
 
 export default {
-  components: { ImageFetcher },
-
+  components: { SingleImage },
   props: ['images'],
-
-  data () {
-    return {
-      loading: false,
-    }
-  },
-
-  mounted () {
-    this.loadImages();
-  },
-
-  methods: {
-    alt (image) {
-      return image.alt;
-    },
-
-    src (image) {
-      return image.small.src;
-    },
-
-    srcset (image) {
-      return `${image.medium.src} ${image.medium.width}w, ${image.large.src} ${image.large.width}w`;
-    },
-
-    loadImages () {
-      this.loading = true;
-      imagesLoaded(this.$el.querySelectorAll('.media__image'), this, instance => {
-        this.loading = false;
-      });
-    },
-  },
 }
 </script>
 
 <style lang="stylus">
 @import '../../../stylus/_config/'
 
-// TODO: refactor image dimensions once grid layout is started...
-.images
+.images__group
   align-items center
   display flex
   height 100%
   justify-content center
-
-.image
-  height 100%
-
-// Invert typical responsive image properties
-.image img
-  height 100%
-  max-width none
-  width auto
-
-.placeholder
-  animation fadeIn .5s ease
-  filter blur(5px)
-
-.hi-res
-  animation fadeIn .5s ease
 
 </style>
---
+++ source/js/vue/components/SingleImage.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+  figure.image(
+    :style="placeholder(image)",
+  )
+    //- img.image__photo(
+    //-   v-if="!loading",
+    //-   :alt="image.alt",
+    //-   :src="image.small.src",
+    //-   :srcset="srcset(image)",
+    //- )
+</template>
+
+<script>
+import imagesLoaded from 'imagesloaded';
+// import ImageFetcher from './ImageFetcher.vue';
+
+export default {
+//   components: { ImageFetcher },
+
+  props: ['image'],
+
+  data () {
+    return {
+      loading: true, // override null while we test...
+    }
+  },
+
+  mounted () {
+    // this.loadImages();
+  },
+
+  methods: {
+    placeholder(image) {
+      return `background: url(${image.placeholder}) center center / cover no-repeat`;
+    },
+
+    srcset (image) {
+      return `${image.medium.src} ${image.medium.width}w, ${image.large.src} ${image.large.width}w`;
+    },
+
+    loadImages () {
+      this.loading = true;
+      const self = this;
+      imagesLoaded(this.$el.querySelector('.image__photo'), self, instance => {
+        self.loading = false;
+      });
+    },
+  },
+}
+</script>
+
+<style lang="stylus">
+@import '../../../stylus/_config/'
+
+// TODO: refactor image dimensions once grid layout is started...
+.image
+  height 100%
+
+// .placeholder
+//   animation fadeIn .5s ease
+//   filter blur(5px)
+
+// Invert typical responsive image properties
+.image__photo
+  animation fadeIn .5s ease
+  height 100%
+  max-width none
+  opacity 0 // hide the real image while we test...
+  width auto
+
+</style>
