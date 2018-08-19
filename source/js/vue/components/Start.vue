3b76708098e13bc208bd1923282b79307f9f7f21
Ben Gauslin <ben@gauslin.com>
2018-08-19 18:30:02 -0400

    cover background image wired up

--- source/js/vue/components/Start.vue
+++ source/js/vue/components/Start.vue
@@ -1,16 +1,25 @@
 <template lang="pug">
-  div.cover
+  div.cover(
+    :style="coverImage(content.image)",
+  )
     router-link(
       class="cover__link",
       :to="{ name: 'slide', params: { slug: content.slug, count: 1 } }",
     )
-      h2.cover__heading {{ content.title }}
-      p.cover__summary {{ content.summary }}
+      header.cover__header
+        h2.cover__heading {{ content.title }}
+        p.cover__summary {{ content.summary }}
 </template>
 
 <script>
 export default {
   props: ['content'],
+
+  methods: {
+    coverImage (image) {
+      return `background: url(${image.large.src}) center center / cover no-repeat;`;
+    }
+  }
 }
 </script>
 
@@ -18,15 +27,19 @@ export default {
 @import '../../../stylus/_config/'
 
 .cover
-  align-items center
-  display flex
-  justify-content center
   height 100vh
   width 100vw
 
 .cover__link
-  display block
+  align-items center
+  background-color rgba(DARK_GREY, .85)
+  display flex
+  height 100%
+  justify-content center
   link(WHITE, WHITE, rgba(WHITE, .75), rgba(WHITE, .75))
+  width 100%
+
+.cover__header
   padding 1rem
   text-align center
 
