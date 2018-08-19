76a4fbdde185ffb0d8805d1d6e00959a80292f92
Ben Gauslin <ben@gauslin.com>
2018-08-19 19:28:24 -0400

    more publication styles

--- source/js/vue/components/MediaPublication.vue
+++ source/js/vue/components/MediaPublication.vue
@@ -8,8 +8,12 @@
     a.publication__link(
       :href="publication.url",
     )
-      h2.publication__title {{ publication.title }}
-      h3.publication__publisher {{ publication.publisher }}
+      h2.publication__title(
+        v-html="publication.title",
+      )
+      h3.publication__publisher(
+        v-html="publication.publisher",
+      )
       p.publication__date {{ publication.date }}
 </template>
 
@@ -26,24 +30,53 @@ export default {
 @import '../../../stylus/_config/'
 
 .publication
-  align-items center
-  display flex
-  margin 0 auto
-  max-width 48rem // TODO: use a constant here
+  margin 1rem
 
-.publication__image
-  flex 0 0 50%
+  @media BREAKPOINT_MEDIUM
+    align-items center
+    display flex
+    margin 0 auto
+    padding 0 4rem
+    max-width 60rem // TODO: use constant here
+
+.image.publication__image
+  margin-left 0
+  width 50%
+
+  @media BREAKPOINT_MEDIUM
+    flex 0 0 33.3333%
+    margin-left 2rem
+    width auto
 
 .publication__link
-  margin-left 2rem
+  display block
+
+  @media BREAKPOINT_MEDIUM
+    padding 2rem
 
 .publication__link > *
   color WHITE
 
-// .publication__title
+.publication__title
+  font-size H2_BASE
+  heading_font()
+  margin 0 0 .5em
 
-// .publication__publisher
+  @media BREAKPOINT_MEDIUM
+    font-size H2_MEDIUM
 
-// .publication__date
+.publication__publisher
+  font-size H3_BASE
+  margin 0 0 .5em
+
+  @media BREAKPOINT_MEDIUM
+    font-size H3_MEDIUM
+
+.publication__date
+  font-size CAPTION_BASE
+  margin .7rem 0 0
+
+  @media BREAKPOINT_MEDIUM
+    font-size CAPTION_MEDIUM
 
 </style>
