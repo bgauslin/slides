8a30015b7b7a1c88f3a9d9d9cbf30ca7d2613b0f
Ben Gauslin <ben@gauslin.com>
2018-09-02 19:25:01 -0400

    updates home page to use SingleImage component

--- source/js/vue/components/Home.vue
+++ source/js/vue/components/Home.vue
@@ -7,50 +7,49 @@
         class="slideshow__link",
         :to="{ name: 'cover', params: { slideshow: slideshow.slug } }",
       )
-        img(
-          :src="slideshow.image.src",
+        single-image(
+          v-if="slideshow.image",
+          :className="imageClass",
+          :image="slideshow.image",
         )
         h2.slideshow__heading {{ slideshow.title }}
 </template>
 
 <script>
+import SingleImage from './SingleImage.vue';
 
 export default {
   props: ['content'],
+  components: { SingleImage },
 }
 </script>
 
 <style lang="stylus">
 @import '../../../stylus/_config/'
 
+// TODO: update slideshows html/css layout similar to thumbs
 .slideshows
-  margin 0 auto
   max-width CONTENT_MAX_WIDTH
-  padding HEADER_HEIGHT 0 0
+  padding HEADER_HEIGHT px_to_rem(16) 0
 
   @media BREAKPOINT_MEDIUM
-    align-items center
     grid-row 2
-    justify-items center
-    padding 0
+    padding 0 COLUMN_GAP
+
+.slideshow
+  margin 0 0 px_to_rem(16)
 
 .slideshow__link
   display block
-  padding 1rem
   transition color DEFAULT_TRANSITION
 
 .slideshow__heading
   font-size H3_SIZE
   heading_font()
-  margin 0 0 .25em
-  padding 0
-
-.slideshow__summary
-  font-size CAPTION_SIZE
-  margin 0
+  margin px_to_em(12) 0
   padding 0
 
   @media BREAKPOINT_MEDIUM
-    font-size CAPTION_SIZE_MEDIUM
+    font-size H3_SIZE_MEDIUM
 
 </style>
