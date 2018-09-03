c8c1f581903655fc15d38a6810184268d2fdc24d
Ben Gauslin <ben@gauslin.com>
2018-09-03 19:30:11 -0400

    clicking on slide goes to next slide (only works with images currently, not publications) - fixes #17

--- source/js/vue/components/Slide.vue
+++ source/js/vue/components/Slide.vue
@@ -5,10 +5,15 @@
     div(
       :class="['slide__media', className]"
     )
-      media-images(
-        v-if="content.media.images",
-        :images="content.media.images",
+      router-link(
+        v-if="images && nextSlide",
+        class="slide__media__link",
+        :title="nextSlideLabel",
+        :to="nextSlideRoute",
       )
+        media-images(
+          :images="images",
+        )
       media-publication(
         v-if="content.media.publication",
         :publication="content.media.publication",
@@ -38,9 +43,31 @@ export default {
 
   computed: {
     className () {
-      if (this.content.media.images && this.content.media.images.length > 1) {
+      if (this.images && this.images.length > 1) {
        return 'slide__media--multiple';
       }
+    },
+
+    images () {
+      return this.content.media.images;
+    },
+
+    nextSlide () {
+      return this.$store.getters.slideNext;
+    },
+
+    nextSlideRoute () { 
+      return {
+        name: 'slide',
+        params: {
+          slideshow: this.$route.params.slideshow,
+          slug: this.nextSlide.slug
+        }
+      };
+    },
+
+    nextSlideLabel () {
+      return this.nextSlide.title;
     },
   }
 }
@@ -86,6 +113,12 @@ export default {
 
   @media BREAKPOINT_LARGE
     margin 0
+
+.slide__media__link
+  display block
+
+.slide__media__link:hover
+  cursor default
 
 .slide__caption
   font-size CAPTION_SIZE
