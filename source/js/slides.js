266bc4f3443be46f804e893169b2bb01096f4140
Ben Gauslin <ben@gauslin.com>
2018-09-10 06:43:42 -0400

    Google Analytics wired up with its own account

--- source/js/slides.js
+++ source/js/slides.js
@@ -4,13 +4,13 @@ import viewportHeight from './modules/viewportHeight';
 import './vue/app';
 import googleAnalytics from './modules/googleAnalytics';
 
-// const gaData = {
-//   domain: 'slides.gauslin.com',
-//   id: 'UA-626192-##'
-// }
+const gaData = {
+  domain: 'slides.gauslin.com',
+  id: 'UA-626192-17'
+}
 
 attachFastClick(document.body);
-// googleAnalytics(gaData);
+googleAnalytics(gaData);
 noTouch();
 viewportHeight();
 
