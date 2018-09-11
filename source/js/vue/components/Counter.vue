aa23012d2182c86ce9e1ddb2cdcd83e23100e885
Ben Gauslin <ben@gauslin.com>
2018-09-11 08:03:56 -0400

    counter link equal to height of controls

--- source/js/vue/components/Counter.vue
+++ source/js/vue/components/Counter.vue
@@ -1,10 +1,9 @@
-<template lang="pug">
-  div.counter
-    router-link(
-      class="counter__link",
-      title="View thumbnails",
-      :to="{ name: 'thumbs' }",
-    ) {{ countCurrent }} of {{ countTotal }}
+<template lang="pug">  
+  router-link(
+    class="counter__link",
+    title="View thumbnails",
+    :to="{ name: 'thumbs' }",
+  ) {{ countCurrent }} of {{ countTotal }}
 </template>
 
 <script>
@@ -24,25 +23,21 @@ export default {
 <style lang="stylus">
 @import '../../../stylus/_config/'
 
-.counter
+.counter__link
   align-items center
   display inline-flex
+  font-size CAPTION_SIZE
+  heading_font()
   height CONTROLS_HEIGHT
   justify-content center
   overflow hidden
-
-  @media BREAKPOINT_MEDIUM
-    height CONTROLS_HEIGHT_MEDIUM
-
-.counter__link
-  font-size CAPTION_SIZE
-  heading_font()
   padding 0 px_to_rem(16)
   transition transform DEFAULT_TRANSITION
   white-space nowrap
 
   @media BREAKPOINT_MEDIUM
     font-size CAPTION_SIZE_MEDIUM
+    height CONTROLS_HEIGHT_MEDIUM
 
 .counter__link:active
   transform scale(.9)
