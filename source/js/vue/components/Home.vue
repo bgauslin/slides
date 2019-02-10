c8899217eeb224ed3149247ddeb1511ee5d2078c
Ben Gauslin <ben@gauslin.com>
2019-02-10 08:33:51 -0500

    markup and styles updates for grid layout on slideshows view

--- source/js/vue/components/Home.vue
+++ source/js/vue/components/Home.vue
@@ -1,20 +1,19 @@
 <template lang="pug">
   div.slideshows
-    div.slideshows__frame
-      div.slideshows__group
-        div.slideshow(
-          v-for="slideshow in content",
+    div.slideshows__group
+      div.slideshow(
+        v-for="slideshow in content",
+      )
+        router-link(
+          class="slideshow__link",
+          :to="{ name: 'cover', params: { slideshow: slideshow.slug } }",
         )
-          router-link(
-            class="slideshow__link",
-            :to="{ name: 'cover', params: { slideshow: slideshow.slug } }",
+          single-image(
+            v-if="slideshow.image",
+            :className="imageClass",
+            :image="slideshow.image",
           )
-            single-image(
-              v-if="slideshow.image",
-              :className="imageClass",
-              :image="slideshow.image",
-            )
-            h2.slideshow__heading {{ slideshow.title }}
+          h2.slideshow__heading {{ slideshow.title }}
 </template>
 
 <script>
