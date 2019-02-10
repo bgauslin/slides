a4bcfb8724e4d1c8c14702cf7a8751f2b56e7104
Ben Gauslin <ben@gauslin.com>
2019-02-10 08:39:34 -0500

    .thumbs__frame removed without css properties moved to thumbs__group

--- source/js/vue/components/Thumbs.vue
+++ source/js/vue/components/Thumbs.vue
@@ -1,14 +1,13 @@
 <template lang="pug">
   div.thumbs
-    div.thumbs__frame
-      div.thumbs__group
-        thumb(
-          v-for="(thumb, index) in content.thumbs",
-          :index="index",
-          :slideshow="content.slug",
-          :slug="thumb.slug",
-          :thumb="thumb.thumb",
-        )
+    div.thumbs__group
+      thumb(
+        v-for="(thumb, index) in content.thumbs",
+        :index="index",
+        :slideshow="content.slug",
+        :slug="thumb.slug",
+        :thumb="thumb.thumb",
+      )
 </template>
 
 <script>
