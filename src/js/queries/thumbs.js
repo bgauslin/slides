0516109148b392ce6150ecd2e241ae930dd79312
Ben Gauslin <ben@gauslin.com>
2019-09-15 19:46:49 -0400

    'thumbs' query refactored with fragments

--- src/js/queries/thumbs.js
+++ src/js/queries/thumbs.js
@@ -2,43 +2,53 @@
 const thumbSize = 160;
 
 /** @const {string} */
-const thumbs = `query ($slideshow: [String!]) {
+const thumbs = `
+query ($slideshow: [String!]) {
   slideshow: entries(section: "slides", type: ["slide", "slideDeck"], slug: $slideshow) {
-    ...on slides_slideDeck_Entry {
-      title
-      id
-      slug
-    }
+    ...SlideDeck
     slides: children {
-      ...on slides_slide_Entry {
-        title
-        id
-        slug
-        media: slideshowMedia {
-          ...on slideshowMedia_images_BlockType {
-            images {
-              ...on slides_Asset {
-                alt: title
-                src: url @transform(width: ${thumbSize}, height: ${thumbSize}, immediately: true)
-              }
-            }
-          }
-          ...on slideshowMedia_publication_BlockType {
-            publication {
-              ...on publications_publication_Entry {
-                images: publicationPhoto {
-                  ...on publications_Asset {
-                    alt: title
-                    src: url @transform(width: ${thumbSize}, height: ${thumbSize}, immediately: true)
-                  }
-                }
-              }
-            }
-          }
-        }
+      ...Slide
+    }
+  }
+}
+
+fragment SlideDeck on slides_slideDeck_Entry {
+  title
+  id
+  slug
+}
+
+fragment Slide on slides_slide_Entry {
+  title
+  id
+  slug
+  media: slideshowMedia {
+    ...Images
+    ...on slideshowMedia_publication_BlockType {
+      publication {
+        ...Publication
       }
     }
   }
-}`;
+}
+
+fragment Images on slideshowMedia_images_BlockType {
+  images {
+    ...on slides_Asset {
+      alt: title
+      src: url @transform(width: ${thumbSize}, height: ${thumbSize}, immediately: true)
+    }
+  }
+}
+
+fragment Publication on publications_publication_Entry {
+  images: publicationPhoto {
+    ...on publications_Asset {
+      alt: title
+      src: url @transform(width: ${thumbSize}, height: ${thumbSize}, immediately: true)
+    }
+  }
+}
+`;
 
 export default thumbs;
