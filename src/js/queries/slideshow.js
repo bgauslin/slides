319386ee582af38f46301a291179aba3d3fefc7d
Ben Gauslin <ben@gauslin.com>
2019-09-16 06:11:19 -0400

    'slideshow' query refactoring with fragments

--- src/js/queries/slideshow.js
+++ src/js/queries/slideshow.js
@@ -18,29 +18,36 @@ const ImageWidth = {
 
 /** @const {string} */
 const slideshow = `
+query ($slideshow: [String!]) {
+  slideshow: entries(section: "slides", type: ["slide", "slideDeck"], slug: $slideshow) {
+    ...SlideDeck
+    slides: children {
+      ...Slide
+    }
+  }
+}
+
+fragment SlideDeck on slides_slideDeck_Entry {
+  title
+  id
+  slug
+  summary
+  image: slideshowCover {
+    ...Cover
+  }
+}
+
 fragment Cover on slides_Asset {
   alt: title
   src: url @transform(width: ${ImageWidth.MEDIUM}, height: ${ImageHeight.MEDIUM}, immediately: true)
   placeholder: url @transform(width: ${ImageWidth.PLACEHOLDER}, height: ${ImageHeight.PLACEHOLDER}, immediately: true)
 }
 
-query ($slideshow: [String!]) {
-  slideshow: entries(section: "slides", type: ["slide", "slideDeck"], slug: $slideshow) {
-    ...on slides_slideDeck_Entry {
-      title
-      id
-      slug
-      summary
-      image: slideshowCover {
-        ...Cover
-      }
-    }
-    slides: children {
-      title
-      id
-      slug
-    }
-  }
-}`;
+fragment Slide on slides_slide_Entry {
+  title
+  id
+  slug
+}
+`;
 
 export default slideshow;
