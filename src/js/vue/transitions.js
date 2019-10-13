2d9133e52069dec4dcb991f503aa217391a8ccc0
Ben Gauslin <ben@gauslin.com>
2019-10-13 07:39:20 -0400

    New and improved transition direction via objects, arrays, and loops. It's more code, but it's also easy to read and maintain

--- src/js/vue/transitions.js
+++ src/js/vue/transitions.js
@@ -1,26 +1,52 @@
 export default (router) => {
   router.beforeEach((to, from, next) => {
-    let direction;
+    let direction = 'first-run';
 
-    if (from.name === null) {
-      direction = 'first-run';
-    } else if ((from.name === 'home' || from.name === 'cover') && (to.name === 'cover' || to.name === 'slide')) {
-      direction = 'forward';
-    } else if ((from.name === 'home' || from.name === 'cover' || from.name === 'slide') && to.name === 'thumbs') {
-      direction = 'forward';
-    } else if ((from.name === 'cover' || from.name === 'slide') && (to.name === 'home' || to.name === 'cover')) {
-      direction = 'back';
-    } else if (from.name === 'thumbs' && (to.name === 'home' || to.name === 'cover' || to.name === 'slide')) {
-      direction = 'back';
-    } else if (from.name === 'slide' && to.name === 'slide') {
-      // Get all slides, find the 'to' slide and get its index.
+    // If we're moving from slide to slide, first get all slides, find the
+    // indexes of the 'to' and 'from' slides, then compare their values and set
+    // 'direction' accordingly.
+    if (from.name === 'slide' && to.name === 'slide') {
       const slides = router.app.$store.getters.slideshow.slides;
       const toSlide = slides.find(slide => slide.slug == to.params.slug);
       const toIndex = slides.indexOf(toSlide);
-      // Get index of the current slide.
       const fromIndex = router.app.$store.getters.slideIndex;
-      // Compare 'current' and 'to' indexes to set direction.
       direction = (toIndex - fromIndex == 1) ? 'forward' : 'back';
+
+    // Otherwise, set direction based on other to/from route combination.
+    } else {
+      const transition = [
+        {
+          direction: 'forward',
+          toFrom: [
+            ['cover', 'home'],
+            ['slide', 'cover'],
+            ['thumbs', 'slide'],
+          ]
+        },
+        {
+          direction: 'back',
+          toFrom: [
+            ['cover', 'slide'],
+            ['cover', 'thumbs'],
+            ['home', 'cover'],
+            ['slide', 'thumbs'],
+          ]
+        }
+      ];
+
+      let i = 0;
+      while (i < transition.length) {
+        const group = transition[i];
+        let j = 0;
+        while (j < group.toFrom.length) {
+          const [to_, from_] = group.toFrom[j];
+          if (to.name === to_ && from.name == from_) {
+            direction = group.direction;
+          }
+          j++;
+        }
+        i++;
+      }
     }
 
     if (router.app.$store) {
