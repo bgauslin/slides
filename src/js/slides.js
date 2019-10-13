c1428ccff928edce069c4ee60ae9d107a0a54b58
Ben Gauslin <github@gauslin.com>
2019-10-13 10:24:46 -0400

    Cleans up slides.js entry file

--- src/js/slides.js
+++ src/js/slides.js
@@ -1,24 +1,17 @@
 require('dotenv').config();
+
 import setup from './setup';
 import Vue from 'vue/dist/vue.js';
 import App from './vue/components/App.vue';
 import router from './vue/router.js';
 import store from './vue/store/index.js';
 import transitions from './vue/transitions';
-
-/** Import CSS for injection via JS. */
 import '../stylus/slides.styl';
 
 Vue.config.productionTip = false;
+transitions(router);
 setup.init();
 
-/** Bind transitions and router. */
-transitions(router);
-
-/**
- * Initialize app, pass it the store and router, and mount it to the DOM.
- * @instance
- */
 new Vue({
   components: { App },
   router,
