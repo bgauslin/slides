e287262064e3edd9e5db81331e9ade989e494a69
Ben Gauslin <ben@gauslin.com>
2019-10-14 06:36:44 -0400

    notFound html and css updates

--- src/js/vue/components/NotFound.vue
+++ src/js/vue/components/NotFound.vue
@@ -1,16 +1,7 @@
 <template lang="pug">
   div.error-404
-    p.error-404__message {{ errorMessage }}
+    h2.error-404__heading 404
+    div.error-404__message
+      p That page cannot be found. :(
+      p Go to the <a href="/">Home page</a>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      errorMessage: '404',
-      summary: 'That page cannot be found. :(',
-      title: 'Uh oh!',
-    }
-  },
-}
-</script>
