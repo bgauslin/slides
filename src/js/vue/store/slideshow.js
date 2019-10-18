e9c31610ce7dd386a8719b98120bb0ed4b03b314
Ben Gauslin <ben@gauslin.com>
2019-10-18 07:43:11 -0400

    TODO added to state.slideshow for controls throwing an error relative to slideIndex value

--- src/js/vue/store/slideshow.js
+++ src/js/vue/store/slideshow.js
@@ -54,6 +54,8 @@ const slideshow = {
       }
     },
 
+    // TODO: Debug slideIndex so that Controls don't throw an error when
+    // starting on an individual slide.
     slideIndex: (state, getters) => {
       if (getters.hasSlideshow) {
         const slide = getters.slide;
