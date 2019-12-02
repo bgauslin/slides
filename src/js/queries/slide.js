dbc6f064b9a804e76b6fa98dc9baadb1118f3ccc
Ben Gauslin <ben@gauslin.com>
2019-12-01 20:22:09 -0500

    slideshow slide images sized via their widths instead of heights for better srcset/sizes coordination

--- src/js/queries/slide.js
+++ src/js/queries/slide.js
@@ -1,4 +1,4 @@
-import { ImageHeight, PublicationWidth } from './imageSizing';
+import { ImageWidth, PublicationWidth } from './imageSizing';
 
 /** @const {string} */
 const slide = `
@@ -27,10 +27,10 @@ fragment Images on slideshowMedia_images_BlockType {
   images {
     ...on slides_Asset {
       alt: title
-      src_small: url @transform(height: ${ImageHeight.SMALL}, interlace: "partition", immediately: true)
-      src_medium: url @transform(height: ${ImageHeight.MEDIUM}, interlace: "partition", immediately: true)
-      src_large: url @transform(height: ${ImageHeight.LARGE}, interlace: "partition", immediately: true)
-      placeholder: url @transform(height: ${ImageHeight.PLACEHOLDER}, interlace: "partition", immediately: true)
+      src_small: url @transform(width: ${ImageWidth.SMALL}, interlace: "partition", immediately: true)
+      src_medium: url @transform(width: ${ImageWidth.MEDIUM}, interlace: "partition", immediately: true)
+      src_large: url @transform(width: ${ImageWidth.LARGE}, interlace: "partition", immediately: true)
+      placeholder: url @transform(width: ${ImageWidth.PLACEHOLDER}, interlace: "partition", immediately: true)
       height
       width   
     }
