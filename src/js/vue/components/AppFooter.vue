c69fa8b8d4812a3aafb5df0ad6c3f6c7e86f81c3
Ben Gauslin <bgauslin@gmail.com>
2021-02-18 18:41:28 -0500

    removes GitHub link from footer

--- src/js/vue/components/AppFooter.vue
+++ src/js/vue/components/AppFooter.vue
@@ -1,22 +1,13 @@
 <template lang="pug">
   footer(:class="['footer', 'footer--' + modifier]")
     p.footer__blurb.footer__blurb--copyright
-      span.footer__years © 2018–{{ currentYear }}
       a.footer__link(
         :href="linkUrl",
         :title="'Visit ' + domain",
         target="_blank",
         rel="noopener",
-      ) Ben Gauslin    
-    p.footer__blurb.footer__blurb--repo(
-      v-if="hasGitHubLink"
-    )
-      a.footer__link(
-        :href="gitHubUrl",
-        title="View source code on GitHub",
-        target="_blank",
-        rel="noopener",
-      ) GitHub
+      )
+        span.footer__years © {{ currentYear }} Ben Gauslin
 </template>
 
 <script>
@@ -29,19 +20,13 @@ export default {
     return {
       domain: 'Gauslin.com',
       owner: 'Ben Gauslin',
-      gitHubUrl: 'https://github.com/bgauslin/slides',
     }
   },
 
   computed: {
     /** @return {string} */
     currentYear() {
-      return new Date().getFullYear().toString().substr(-2);
-    },
-
-    /** @return {string} */
-    hasGitHubLink() {
-      return this.modifier === 'app';
+      return new Date().getFullYear();
     },
 
     /** @return {string} */
